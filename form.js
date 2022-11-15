04e9ffd53cebb40a2d80fe06f539faf615d4abea
Arun4Rajbhar <89437068+Arun4Rajbhar@users.noreply.github.com>
2022-11-15 15:59:20 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,13 @@
+function display(){
+    var  fname=document.getElementById("fname").value;
+    document.getElementById("fname1").innerHTML=fname;
+
+    var  lname=document.getElementById("lname").value;
+    document.getElementById("lname1").innerHTML=lname;
+
+    var  city=document.getElementById("select_city").value;
+    document.getElementById("city").innerHTML=city;
+
+    // var  fname=document.getElementById("fname").value;
+    // document.getElementById("fname1").innerHTML=fname;
+}
